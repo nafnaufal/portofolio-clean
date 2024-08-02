ee9307ceb04a54d8d020ec1965799333183d2a9e
nafnaufal <naufalanbial@gmail.com>
2024-08-02 20:32:03 +0700

    init

--- src/pages/Project.js
+++ src/pages/Project.js
@@ -1,4 +1,4 @@
-import { Grid, GridItem, Heading, Flex, Image, Text, IconButton, Button, Spacer } from '@chakra-ui/react'
+import { Grid, GridItem, Heading, Flex, Image, Text, Button, Link } from '@chakra-ui/react'
 import { BsBoxArrowUpRight } from "react-icons/bs";
 import { LiaLinkedinIn, LiaHackerrank, LiaGithub } from "react-icons/lia";
 import { TbBrandLeetcode } from "react-icons/tb";
@@ -10,175 +10,103 @@ import { useState } from 'react';
 
 function Project() {
 
-	// Color #EEEDEB #E6B9A6 #939185 #2F3645
-	// Color red#FF1E00 teal#E8F9FD cyan#59CE8F black#000000
-	// change color, too distracting
-	let data = project
+    // Color #EEEDEB #E6B9A6 #939185 #2F3645
+    // Color red#FF1E00 teal#E8F9FD cyan#59CE8F black#000000
+    // change color, too distracting
+    let data = project
 
-	const [sel, setSel] = useState(0)
-	return (
-		<Grid
-			h='100vh'
-			templateRows='repeat(14, 1fr)'
-			templateColumns='repeat(10, 1fr)'
-			gap={1}
-			bg={'#EEEDEB'}
-			color={'#000000'}
-			pb={'16'}
-			pl={'8'}
-			pr={'40'}
-		>
-			<GridItem className='title' rowSpan={14} colSpan={2} mb="4">
-				<Grid
-					h='100%'
-					w={'100%'}
-					templateRows='repeat(14, 1fr)'
-					templateColumns='repeat(2, 1fr)'
-					gap={1}
-				>
-					<GridItem className='title' rowSpan={10} colSpan={2} mb="4">
-						<Flex height={'100%'} justifyContent={"end"} alignItems={'end'} color={"#000000"}>
-							<Heading size='xl' textAlign="right" lineHeight={'1'} textTransform={'uppercase'} css={css`
-						writing-mode:vertical-lr;
-						`} transform={"rotate(180deg)"} color={"#FF1E00"}>naufal</Heading>
-							<Heading size='xl' textAlign="left" lineHeight={'1.5'} textTransform={'uppercase'} css={css`
-						writing-mode:vertical-lr;
-						`} transform={"rotate(180deg)"} h={"100%"}>anbial falah</Heading>
-						</Flex>
-					</GridItem>
-				</Grid>
-			</GridItem>
-			<GridItem className='show-project' rowSpan={10} colSpan={6} >
-				<Grid
-					templateRows='repeat(10, 1fr)'
-					templateColumns='repeat(6, 1fr)'
-					height={"100%"}
-					width={"100%"}
-				>
+    const [sel, setSel] = useState(0)
+    return (
+        <Grid
+            h='100vh'
+            templateRows='repeat(14, 1fr)'
+            templateColumns='repeat(10, 1fr)'
+            gap={1}
+            bg={'#EEEDEB'}
+            color={'#000000'}
+            py={'4'}
+            px={'24'}
+        >
+            <GridItem className='title' rowSpan={2} colSpan={10} mb="4">
+                <Flex justifyContent={'space-between'} alignItems={'center'}>
+                    <Flex flexDirection={'column'} alignItems={'start'}>
+                        <Link color={'#FF1E00'}>profile</Link>
+                        <Link >project</Link>
+                    </Flex>
+                    <Text fontWeight={"bold"}>naufal anbial falah</Text>
+                    <Flex flexDirection={'column'} alignItems={'end'}>
+                        <Link >contact me!</Link>
+                        <Flex flexDirection={'row'} >
+                            <a href='https://www.linkedin.com/in/nafnaufal/	'><LiaLinkedinIn /></a>
+                            <a href='https://github.com/nafnaufal'><LiaGithub /></a>
+                            <a href='https://leetcode.com/u/nafnaufal/'><TbBrandLeetcode /></a>
+                            <a href='https://www.hackerrank.com/profile/naufalanbial'><LiaHackerrank /></a>
+                        </Flex>
+                    </Flex>
+                </Flex>
+            </GridItem>
 
-					<GridItem className='show-project' rowSpan={2} colSpan={6} mx="4">
-						<Flex flexDirection={"row"} w={'100%'} h={'100%'} justifyContent={"end"} alignItems={"end"}>
-							<a href='https://www.linkedin.com/in/nafnaufal/	'><IconButton css={
-								css`
-									&:hover{
-										background-color: #000000;
-										color: #E8F9FD;
-									}
-								`
-							} borderRadius={"0 0 0 0"} colorScheme='black' variant='outline' aria-label='Linkedin' icon={<LiaLinkedinIn />} /></a>
-							<a href='https://github.com/nafnaufal'><IconButton css={
-								css`
-									&:hover{
-										background-color: #000000;
-										color: #E8F9FD;
-									}
-								`
-							} borderRadius={"0 0 0 0"} colorScheme='black' variant='outline' aria-label='Github' icon={<LiaGithub />} /></a>
-							<a href='https://leetcode.com/u/nafnaufal/'><IconButton css={
-								css`
-									&:hover{
-										background-color: #000000;
-										color: #E8F9FD;
-									}
-								`
-							} borderRadius={"0 0 0 0"} colorScheme='black' variant='outline' aria-label='Leetcode' icon={<TbBrandLeetcode />} /></a>
-							<a href='https://www.hackerrank.com/profile/naufalanbial'><IconButton css={
-								css`
-									&:hover{
-										background-color: #000000;
-										color: #E8F9FD;
-									}
-								`
-							} borderRadius={"0 0 0 0"} colorScheme='black' variant='outline' aria-label='Hackerrank' icon={<LiaHackerrank />} /></a>
-						</Flex>
+            <GridItem className='show-project' rowSpan={10} colSpan={8} >
 
-					</GridItem>
+                <Flex flexDirection={"column"} height={'100%'} justifyContent={"center"} border={"solid 1px"}>
+                    <Image
+                        height={'100%'}
+                        width={'100%'}
+                        objectFit='cover'
+                        src={data[sel]["img"][0]}
+                        alt={data[sel]["title"]}
+                        css={css`
+                                        filter: grayscale(100%);
+                                        transition: filter 0.3s;
+                                        &:hover {
+                                            filter: none;
+                                        }
+                                    `}
+                    />
+                </Flex>
+            </GridItem>
 
-					<GridItem className='project-detail' bg={'#2F3645'} rowSpan={8} colSpan={6} m="4">
-						<Flex flexDirection={"column"} height={'100%'} justifyContent={"center"} border={"solid 1px"}>
-							<Image
-								height={'100%'}
-								width={'100%'}
-								objectFit='cover'
-								src={data[sel]["img"][0]}
-								alt={data[sel]["title"]}
-								css={css`
-										filter: grayscale(100%);
-										transition: filter 0.3s;
-										&:hover {
-											filter: none;
-										}
-									`}
-							/>
-						</Flex>
-					</GridItem>
-				</Grid>
-			</GridItem>
+            <GridItem className='profile-detail' rowSpan={14} colSpan={2}>
+                <Grid
+                    templateRows='repeat(14, 1fr)'
+                    templateColumns='repeat(1, 1fr)'
+                    height={"100%"}
+                    width={"100%"}
+                >
 
-			<GridItem className='profile-detail' rowSpan={14} colSpan={2}>
-				<Grid
-					templateRows='repeat(14, 1fr)'
-					templateColumns='repeat(1, 1fr)'
-					height={"100%"}
-					width={"100%"}
-				>
+                    <GridItem rowSpan={2} colSpan={1}>
 
-					<GridItem rowSpan={2} colSpan={1}>
-
-						<Flex flexDirection={"row"} h={'100%'} w={'100%'} justifyContent={"start"} alignItems={"end"} >
-							<Button css={
-								css`
-									&:hover{
-										background-color: #000000;
-										color: #E8F9FD;
-									}
-								`
-							} borderRadius={"0 0 0 0"} colorScheme='black' variant='outline'>
-								Profile
-							</Button>
-							<Button css={
-								css`
-									&:hover{
-										background-color: #000000;
-										color: #E8F9FD;
-									}
-								`
-							} borderRadius={"0 0 0 0"} colorScheme='black' variant='outline'>
-								Contact
-							</Button>
-
-						</Flex>
-					</GridItem>
-					<GridItem rowSpan={8} colSpan={1} my="4">
-						<Flex flexDirection={"column"} height={'20%'} justifyContent={"start"} alignItems={"start"} fontSize='sm'>
-							<Flex flexDirection={"row"} justifyContent={"start"} >
-								<Text fontSize={"lg"} fontWeight={'bold'} textAlign={'left'} mr={"2"}>{data[sel]["title"]}</Text>
-								<Flex flexDirection={"row"} alignItems={"center"} height={"100%"} >
-									<BsBoxArrowUpRight />
-								</Flex>
-							</Flex>
-							<Text textAlign={'left'} mb={'4'}>{data[sel]["tags"]}</Text>
-							<Text textAlign={'justify'}>{data[sel]["desc"]}</Text>
-						</Flex>
-					</GridItem>
-				</Grid>
-			</GridItem>
-			{/* show swiss style box and circle when change */}
-			{/* or parallax on box */}
+                    </GridItem>
+                    <GridItem rowSpan={8} colSpan={1} my="4">
+                        <Flex flexDirection={"column"} height={'20%'} justifyContent={"start"} alignItems={"start"} fontSize='sm'>
+                            <Flex flexDirection={"column"} justifyContent={"start"} >
+                                <BsBoxArrowUpRight />
+                                <Link href='https://chakra-ui.com' isExternal>
+                                    {data[sel]["title"]}
+                                </Link>
+                            </Flex>
+                            <Text textAlign={'left'} mb={'4'}>{data[sel]["tags"]}</Text>
+                            <Text textAlign={'justify'}>{data[sel]["desc"]}</Text>
+                        </Flex>
+                    </GridItem>
+                </Grid>
+            </GridItem>
+            {/* show swiss style box and circle when change */}
+            {/* or parallax on box */}
 
 
-			{data.map((item, i) => (
-				<GridItem className='project-item' rowSpan={2} colSpan={2} px="4">
-					<Flex height={"100%"}>
-						<button onClick={() => setSel(i)} >
-							<ProjectItem project={item} sel={sel} pos={i} len={data.length} />
-						</button>
-					</Flex>
-				</GridItem>
-			))}
+            {data.map((item, i) => (
+                <GridItem className='project-item' rowSpan={2} colSpan={2} px="4">
+                    <Flex height={"100%"}>
+                        <button onClick={() => setSel(i)} >
+                            <ProjectItem project={item} sel={sel} pos={i} len={data.length} />
+                        </button>
+                    </Flex>
+                </GridItem>
+            ))}
 
-		</Grid>
-	);
+        </Grid>
+    );
 }
 
 export default Project;
